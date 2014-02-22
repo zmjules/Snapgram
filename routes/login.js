d74a78fd0416888b57fbd999a7f69b9d1c90b2da
Brad Rougeau <brad@Meas-MacBook-Pro.local>
2014-02-21 20:06:01 -0700

    Add database support for registration and login

--- routes/login.js
+++ routes/login.js
@@ -7,8 +7,29 @@ exports.registerPage = function(req, res){
 }
 
 exports.registerAction = function(req, res){
-    req.session.username = req.body.username;
-    res.redirect('/feed');
+    mysql = require('mysql');
+    conn = mysql.createConnection({
+        host: 'localhost',
+        user: 's513_b.rougeau',
+        password: '10013253',
+        database: 's513_b.rougeau'
+    });
+    conn.connect();
+    
+    registerUser = "INSERT INTO User (username, password) VALUES (?, ?)"
+    
+    conn.query(registerUser, [req.body.username, req.body.password], function(err, rows, fields) {
+               if (err) 
+               {
+                console.log(err); 
+                res.redirect("/users/new");
+               }
+               else
+               {
+                req.session.username = req.body.username;
+                res.redirect('/feed');
+               }
+               });
 }
 
 exports.loginPage = function(req, res){
@@ -16,6 +37,27 @@ exports.loginPage = function(req, res){
 };
 
 exports.loginAction = function(req, res){
-    req.session.username = req.body.username;
-    res.redirect('/feed');
+    mysql = require('mysql');
+    conn = mysql.createConnection({
+                                  host: 'localhost',
+                                  user: 's513_b.rougeau',
+                                  password: '10013253',
+                                  database: 's513_b.rougeau'
+                                  });
+    conn.connect();
+    
+    findUser = "SELECT password from User where username = ?"
+    
+    conn.query(findUser, [req.body.username], function(err, rows, fields) {
+               if (err || rows.length < 1 || rows[0].password != req.body.password) 
+               {
+               console.log("Failed"); 
+               res.redirect("/sessions/new");
+               }
+               else
+               {
+               req.session.username = req.body.username;
+               res.redirect('/feed');
+               }
+               });
 }
