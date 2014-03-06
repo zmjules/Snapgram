b83ae702fc64ca172c2aca843469bd10e6d62c78
Brad Rougeau <brad@Meas-MacBook-Pro.local>
2014-03-06 13:57:51 -0700

    Add script for testing the POSTing of user and photo data

---
+++ testScript.js
@@ -0,0 +1,52 @@
+var http = require('http'),
+ querystring = require('querystring')
+ 
+var jsonUsers = JSON.stringify([{id: 1, name: 'steve', password: '1234'}, {'id': 5, 'name': 'darren', password: '5678'}]);
+
+ var options = {
+   host: 'localhost',
+   port: 3000,
+   path: '/bulk/users?password=zorodi',
+   method: 'POST',
+   headers: {
+        'Content-Type': "application/json; charset=utf-8",
+        'Content-Length': Buffer.byteLength(jsonUsers)
+	}
+ }
+
+ // setup the request
+ console.log('sending request: ' + options.host + options.path)
+ var request = http.request(options, function (res) {
+	res.on('data', function(chunk) {
+		console.log(chunk.toString());
+	});
+ });
+ request.write(jsonUsers);
+
+ // complete the request
+ request.end()
+ 
+ var jsonPhotos = JSON.stringify([{id: 1, user_id: 1, path: '/steve/photo1', timestamp: 1234}, {id: 7, user_id: 1, path: '/steve/photo2', timestamp: 5678}]);
+
+ var options2 = {
+   host: 'localhost',
+   port: 3000,
+   path: '/bulk/photos?password=zorodi',
+   method: 'POST',
+   headers: {
+        'Content-Type': "application/json; charset=utf-8",
+        'Content-Length': Buffer.byteLength(jsonPhotos)
+	}
+ }
+
+ // setup the request
+ console.log('sending request: ' + options2.host + options2.path)
+ var request2 = http.request(options2, function (res) {
+	res.on('data', function(chunk) {
+		console.log(chunk.toString());
+	});
+ });
+ request2.write(jsonPhotos);
+
+ // complete the request
+ request2.end()
